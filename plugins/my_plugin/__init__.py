fe20e234f86ca2030799fc96064a790ac51f38d4
Eklavya Sharma <ekurgn@gmail.com>
2019-07-21 22:32:49 +0530

    Improve my_plugin output

--- plugins/my_plugin/__init__.py
+++ plugins/my_plugin/__init__.py
@@ -11,16 +11,17 @@ def inspect_static_generator(static_generator):
     staticfiles = static_generator.staticfiles
 
 
-def post_process_hook(pelican):
+def post_process_staticfiles(pelican):
+    count = 0
     for sc in staticfiles:
         if (sc.source_path, sc.save_as) not in seen_file_pairs:
-            post_process(input_path=pjoin(pelican.path, sc.source_path),
+            count += post_process(input_path=pjoin(pelican.path, sc.source_path),
                 output_path=pjoin(pelican.output_path, sc.save_as),
                 url=sc.url, settings=pelican.settings)
             seen_file_pairs.add((sc.source_path, sc.save_as))
-    print('post-processed {} static files'.format(len(seen_file_pairs)))
+    print('post-processed {}/{} static files'.format(count, len(seen_file_pairs)))
 
 
 def register():
     signals.static_generator_finalized.connect(inspect_static_generator)
-    signals.finalized.connect(post_process_hook)
+    signals.finalized.connect(post_process_staticfiles)
--- plugins/my_plugin/post_process.py
+++ plugins/my_plugin/post_process.py
@@ -1,15 +1,24 @@
+from __future__ import print_function
+
 import os
+import sys
 import subprocess
 
 
 def post_process_dot(input_path, output_path):
     output_path += '.svg'
     args = ['dot', '-Tsvg', input_path, '-o', output_path]
-    print('post_process_dot: running', args)
-    subprocess.check_call(args)
+    try:
+        subprocess.check_call(args)
+    except Exception:
+        print('An error occurred while running', args, file=sys.stderr)
+        raise
+    return True
 
 
 def post_process(input_path, output_path, url, settings):
     ext = os.path.splitext(input_path)[1]
     if ext == '.dot':
-        post_process_dot(input_path, output_path)
+        return post_process_dot(input_path, output_path)
+    else:
+        return False
